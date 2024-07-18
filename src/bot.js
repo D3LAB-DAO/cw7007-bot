75a48ca7122345a9df51cec428f56034b42b01a3
lukepark327 <lukepark327@gmail.com>
2024-07-18 21:56:48 +0900

    feat: chatgpt integration

--- src/bot.js
+++ src/bot.js
@@ -4,11 +4,13 @@ const { Secp256k1HdWallet } = require("@cosmjs/amino");
 const { SigningCosmWasmClient } = require("@cosmjs/cosmwasm-stargate");
 const { calculateFee, GasPrice } = require("@cosmjs/stargate");
 
+const axios = require('axios');
+
 const express = require('express');
 const cors = require("cors");
 const corsOptions = {
     origin: '*',
-    credentials: true,            //access-control-allow-credentials:true
+    credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
 }
 
@@ -22,6 +24,9 @@ const mnemonic = process.env.MNEMONIC;
 
 const gasPrice = GasPrice.fromString("140000000000aconst");
 const executeFee = calculateFee(300_000, gasPrice);
+
+
+const apiKey = process.env.OPENAI_API_KEY;
 
 
 async function bot() {
@@ -44,8 +49,15 @@ async function bot() {
             const id = ids.ids[j];
             try {
                 // Update description
-                const execRes = await updateDescription(client, senderAddress, id, "World"); // TODO
+                const nftInfo = await getNftInfo(client, id);
+                const query = prompt.prompt + "\n---\n" + nftInfo.extension.description + "Answer very concisely."
+                const answer = await callChatGPT(apiKey, query);
+                console.log(`query: ${nftInfo.extension.description}\nanswer: ${answer}`);
+
+                const execRes = await updateDescription(client, senderAddress, id, answer);
                 console.log(`tx: ${execRes.transactionHash}`);
+                const tokenId = execRes.logs[0].events.find(e => e.type === 'wasm').attributes.find(attr => attr.key === 'token_id').value;
+                console.log('Token ID:', tokenId);
             } catch (error) {
                 console.error(error);
             }
@@ -68,6 +80,30 @@ async function getIds(client) {
         "request_ids": {},
     });
     return queryResult ? queryResult : null;
+}
+
+async function getNftInfo(client, token_id) {
+    const queryResult = await client.queryContractSmart(contractAddress, {
+        "nft_info": {
+            "token_id": token_id
+        },
+    });
+    return queryResult ? queryResult : null;
+}
+
+async function callChatGPT(apiKey, content) {
+    const url = 'https://api.openai.com/v1/chat/completions';
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`,
+    };
+    const body = JSON.stringify({
+        model: 'gpt-4o',
+        messages: [{ role: 'user', content: content }],
+        max_tokens: 150,
+    });
+    const response = await axios.post(url, body, { headers });
+    return response.data.choices[0].message.content;
 }
 
 async function updateDescription(client, senderAddress, token_id, output) {
